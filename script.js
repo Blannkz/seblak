ef641a8054c6d6fb9b0769c2d70571c7ae14f3ed
Blannkz <barusyahrul2@gmail.com>
2025-03-14 20:31:33 +0700

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ document.getElementById("orderForm").addEventListener("submit", function(event) 
     let name = document.getElementById("name").value;
     let menu = document.getElementById("menu").value;
     let qty = document.getElementById("qty").value;
+
+    let orderList = document.getElementById("orderList");
+    let newOrder = document.createElement("li");
+    newOrder.innerText = `${name} memesan ${qty} ${menu}`;
     
-    document.getElementById("orderResult").innerText = 
-        `Terima kasih, ${name}! Pesanan ${qty} ${menu} Anda sedang diproses.`;
+    orderList.appendChild(newOrder);
+    
+    document.getElementById("orderForm").reset();
 });
